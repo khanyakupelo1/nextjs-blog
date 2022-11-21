2069efaf2e8a5ea54c65bc73fecffc91f8284b00
Khanya <dev@kupelo.co.za>
2022-11-21 13:46:55 +0200

    Uses global styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+import styles from '../styles/global.css';
+
+/**
+ * 
+ * @param {*} Component the component common accross all different pages
+ * @returns component
+ */
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
