594b90c2baa707af1efe54cabb1850de4c7c879e
Khanya <dev@kupelo.co.za>
2022-11-21 14:08:51 +0200

    Finished adding assets, metadata and css

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
@@ -9,10 +10,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+
+
         <p> Full stack developer with at least 3 years of professional experience!</p>
         <p>
           On this website you'll find a blogs about why I should start using Next.js instead of React.js
         </p>
+
+        <h1>
+          Checkout <Link href="/posts/first-post">blog 1</Link>
+        </h1>
+
+
+
       </section>
     </Layout>
   );
